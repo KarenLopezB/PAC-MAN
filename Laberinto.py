b8e8c6963ba0aa6495be1f025e7b6a2e1f08d9c2
Karen Daniela López <Karen_lopez1936@hotmail.com>
2021-02-21 12:46:01 -0700

    Laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -2,10 +2,216 @@ from OpenGL.GL import *
 from glew_wish import *
 import glfw
 
-def dibujar():
+def paredes():
     #rutinas de dibujo
-    glBegin(GL_TRIANGLES)
+    glBegin(GL_QUADS)
+    glColor3f(0.0, 0.4, 1.0)
+    #ParedIzquierda
+    glVertex(-1, 1, 0.0)
+    glVertex(-0.98, 1, 0.0)
+    glVertex(-0.98, -1, 0.0)
+    glVertex(-1, -1, 0.0)
+    #ParedDerecha
+    glVertex(0.98, 1, 0.0)
+    glVertex(1, 1, 0.0)
+    glVertex(1, -1, 0.0)
+    glVertex(0.98, -1, 0.0)
+    #ParedArriba
+    glVertex(-0.98, 1, 0.0)
+    glVertex(0.98, 1, 0.0)
+    glVertex(0.98, 0.98, 0.0)
+    glVertex(-0.98, 0.98, 0.0)
+    #ParedAbajo
+    glVertex(-0.98, -0.98, 0.0)
+    glVertex(0.98, -0.98, 0.0)
+    glVertex(0.98, -1, 0.0)
+    glVertex(-0.98, -1, 0.0)
+    #EsquinaIzquierdaArriba
+    glVertex(-0.78, 0.78, 0.0)
+    glVertex(-0.58, 0.78, 0.0)
+    glVertex(-0.58, 0.76, 0.0)
+    glVertex(-0.78, 0.76, 0.0)
+
+    glVertex(-0.78, 0.56, 0.0)
+    glVertex(-0.58, 0.56, 0.0)
+    glVertex(-0.58, 0.54, 0.0)
+    glVertex(-0.78, 0.54, 0.0)
+
+    glVertex(-0.58, 0.78, 0.0)
+    glVertex(-0.56, 0.78, 0.0)
+    glVertex(-0.56, 0.54, 0.0)
+    glVertex(-0.58, 0.54, 0.0)
+    #RayaIzquierdaArriba
+    glVertex(-0.36, 0.98, 0.0)
+    glVertex(-0.34, 0.98, 0.0)
+    glVertex(-0.34, 0.54, 0.0)
+    glVertex(-0.36, 0.54, 0.0)
+    #EsquinaDerechaArriba
+    glVertex(0.76, 0.78, 0.0)
+    glVertex(0.78, 0.78, 0.0)
+    glVertex(0.78, 0.58, 0.0)
+    glVertex(0.76, 0.58, 0.0)
+
+    glVertex(0.54, 0.58, 0.0)
+    glVertex(0.78, 0.58, 0.0)
+    glVertex(0.78, 0.56, 0.0)
+    glVertex(0.54, 0.56, 0.0)
+
+    glVertex(0.54, 0.78, 0.0)
+    glVertex(0.56, 0.78, 0.0)
+    glVertex(0.56, 0.58, 0.0)
+    glVertex(0.54, 0.58, 0.0)
+    #RayaDerechaArriba
+    glVertex(0.32, 0.98, 0.0)
+    glVertex(0.34, 0.98, 0.0)
+    glVertex(0.34, 0.56, 0.0)
+    glVertex(0.32, 0.56, 0.0)
+    #RayaArribadeScore
+    glVertex(-0.98, -0.76, 0.0)
+    glVertex(0.98, -0.76, 0.0)
+    glVertex(0.98, -0.78, 0.0)
+    glVertex(-0.98, -0.78, 0.0)
+    #EsquinaIzquierdaAbajo
+    glVertex(-0.78, -0.36, 0.0)
+    glVertex(-0.76, -0.36, 0.0)
+    glVertex(-0.76, -0.56, 0.0)
+    glVertex(-0.78, -0.56, 0.0)
+
+    glVertex(-0.78, -0.34, 0.0)
+    glVertex(-0.56, -0.34, 0.0)
+    glVertex(-0.56, -0.36, 0.0)
+    glVertex(-0.78, -0.36, 0.0)
     
+    glVertex(-0.56, -0.34, 0.0)
+    glVertex(-0.54, -0.34, 0.0)
+    glVertex(-0.54, -0.56, 0.0)
+    glVertex(-0.56, -0.56, 0.0)
+    #RayaIzquierdaAbajo
+    glVertex(-0.34, -0.34, 0.0)
+    glVertex(-0.32, -0.34, 0.0)
+    glVertex(-0.32, -0.76, 0.0)
+    glVertex(-0.34, -0.76, 0.0)
+    #EsquinaDerechaAbajo
+    glVertex(0.58, -0.34, 0.0)
+    glVertex(0.78, -0.34, 0.0)
+    glVertex(0.78, -0.36, 0.0)
+    glVertex(0.58, -0.36, 0.0)
+
+    glVertex(0.58, -0.56, 0.0)
+    glVertex(0.78, -0.56, 0.0)
+    glVertex(0.78, -0.58, 0.0)
+    glVertex(0.58, -0.58, 0.0)
+
+    glVertex(0.56, -0.34, 0.0)
+    glVertex(0.58, -0.34, 0.0)
+    glVertex(0.58, -0.58, 0.0)
+    glVertex(0.56, -0.58, 0.0)
+    #RayaDerechaAbajo
+    glVertex(0.34, -0.34, 0.0)
+    glVertex(0.36, -0.34, 0.0)
+    glVertex(0.36, -0.76, 0.0)
+    glVertex(0.34, -0.76, 0.0)
+
+    #LineasAbajoCentro
+    glVertex(-0.98, -0.14, 0.0)
+    glVertex(-0.56, -0.14, 0.0)
+    glVertex(-0.56, -0.12, 0.0)
+    glVertex(-0.98, -0.12, 0.0)
+
+    glVertex(-0.36, -0.14, 0.0)
+    glVertex(-0.09, -0.14, 0.0)
+    glVertex(-0.09, -0.12, 0.0)
+    glVertex(-0.36, -0.12, 0.0)
+
+    glVertex(0.54, -0.14, 0.0)
+    glVertex(0.98, -0.14, 0.0)
+    glVertex(0.98, -0.12, 0.0)
+    glVertex(0.54, -0.12, 0.0)
+
+    glVertex(0.11, -0.14, 0.0)
+    glVertex(0.34, -0.14, 0.0)
+    glVertex(0.34, -0.12, 0.0)
+    glVertex(0.11, -0.12, 0.0)
+
+    glVertex(-0.09, -0.34, 0.0)
+    glVertex(0.11, -0.34, 0.0)
+    glVertex(0.11, -0.32, 0.0)
+    glVertex(-0.09, -0.32, 0.0)
+
+    glVertex(-0.34, -0.54, 0.0)
+    glVertex(-0.09, -0.54, 0.0)
+    glVertex(-0.09, -0.56, 0.0)
+    glVertex(-0.34, -0.56, 0.0)
+
+    glVertex(0.11, -0.54, 0.0)
+    glVertex(0.36, -0.54, 0.0)
+    glVertex(0.36, -0.56, 0.0)
+    glVertex(0.11, -0.56, 0.0)
+
+    #CasitaFantasmasPacman
+    glVertex(-0.34, 0.34, 0.0)
+    glVertex(-0.09, 0.34, 0.0)
+    glVertex(-0.09, 0.32, 0.0)
+    glVertex(-0.34, 0.32, 0.0)
+
+    glVertex(0.11, 0.34, 0.0)
+    glVertex(0.34, 0.34, 0.0)
+    glVertex(0.34, 0.32, 0.0)
+    glVertex(0.11, 0.32, 0.0)
+
+    glVertex(-0.34, 0.12, 0.0)
+    glVertex(0.34, 0.12, 0.0)
+    glVertex(0.34, 0.10, 0.0)
+    glVertex(-0.34, 0.10, 0.0)
+
+    glVertex(-0.36, 0.34, 0.0)
+    glVertex(-0.34, 0.34, 0.0)
+    glVertex(-0.34, 0.10, 0.0)
+    glVertex(-0.36, 0.10, 0.0)
+
+    glVertex(0.32, 0.34, 0.0)
+    glVertex(0.34, 0.34, 0.0)
+    glVertex(0.34, 0.10, 0.0)
+    glVertex(0.32, 0.10, 0.0)
+
+    #RayasLadoIzquierdo
+    glVertex(-0.98, 0.34, 0.0)
+    glVertex(-0.56, 0.34, 0.0)
+    glVertex(-0.56, 0.32, 0.0)
+    glVertex(-0.98, 0.32, 0.0)
+
+    glVertex(-0.78, 0.12, 0.0)
+    glVertex(-0.56, 0.12, 0.0)
+    glVertex(-0.56, 0.10, 0.0)
+    glVertex(-0.78, 0.10, 0.0)
+
+    #RayasLadoDerecho
+    glVertex(0.54, 0.34, 0.0)
+    glVertex(0.98, 0.34, 0.0)
+    glVertex(0.98, 0.32, 0.0)
+    glVertex(0.54, 0.32, 0.0)
+
+    glVertex(0.54, 0.12, 0.0)
+    glVertex(0.78, 0.12, 0.0)
+    glVertex(0.78, 0.10, 0.0)
+    glVertex(0.54, 0.10, 0.0)
+
+    #RayasArribaCentro
+    glVertex(-0.34, 0.78, 0.0)
+    glVertex(-0.09, 0.78, 0.0)
+    glVertex(-0.09, 0.76, 0.0)
+    glVertex(-0.34, 0.76, 0.0)
+
+    glVertex(0.11, 0.78, 0.0)
+    glVertex(0.34, 0.78, 0.0)
+    glVertex(0.34, 0.76, 0.0)
+    glVertex(0.11, 0.76, 0.0)
+
+    glVertex(-0.09, 0.56, 0.0)
+    glVertex(0.11, 0.56, 0.0)
+    glVertex(0.11, 0.54, 0.0)
+    glVertex(-0.09, 0.54, 0.0)
+
     glEnd()
 
 def main():
@@ -15,7 +221,7 @@ def main():
     
     #crea la ventana, 
     # independientemente del SO que usemos
-    window = glfw.create_window(800,800,"Mi ventana", None, None)
+    window = glfw.create_window(700,700,"Mi ventana", None, None)
 
     #Configuramos OpenGL
     glfw.window_hint(glfw.SAMPLES, 4)
@@ -49,14 +255,14 @@ def main():
 
     while not glfw.window_should_close(window):
         #Establece regiond e dibujo
-        glViewport(0,0,800,800)
+        glViewport(0,0,700,700)
         #Establece color de borrado
-        glClearColor(0.4,0.8,0.1,1)
+        glClearColor(0,0,0,1)
         #Borra el contenido de la ventana
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         #Dibujar
-        dibujar()
+        paredes()
 
         #Preguntar si hubo entradas de perifericos
         #(Teclado, mouse, game pad, etc.)
